50dae99bcb9a40addd3aab73114db235e768c990
povzlo <povzlo.eugene@gmail.com>
2024-10-02 17:13:17 +0300

    announces

--- src/sxastarter/src/components/NewsAndEvents/NewsAnnounce.tsx
+++ src/sxastarter/src/components/NewsAndEvents/NewsAnnounce.tsx
@@ -3,33 +3,38 @@ import React from 'react';
 import { Field, RichText, Text, Link, LinkField } from '@sitecore-jss/sitecore-jss-nextjs';
 
 //Define fields that our component will get during rendering. In this component we render tree fields: Title, Link, Content
+interface Fields {
+  Title: Field<string>;
+  PageLink: LinkField;
+  Content: Field<string>;
+}
 type EventAnnouncesProps = {
-  fields: {
-    Title: Field<string>;
-    Link: LinkField;
-    Content: Field<string>;
-  };
+  fields: Fields;
+  id: string;
 };
 
-export const Default = ({ fields }: EventAnnouncesProps): JSX.Element => {
+export const Default = (props: EventAnnouncesProps): JSX.Element => {
+  const id = props.id;
   return (
     <section className="sc-applicationContent">
-      <div className="col-md-12 sc-applicationContent-main">
+      <div className="col-md-12 sc-applicationContent-main" id={id ? id : undefined}>
         <div>
           <div className="col-md-9">
             <h3>News Announce:</h3>
           </div>
           <div className="col-md-9">
             <h3>
-              {/* Define editalbe text field to render Title field */}
-              <Text field={fields?.Title} />
-              {/* Define editalbe link field to render Link field */}
-              <Link field={fields?.Link} />
+              {/* Render PageLink link field if it is not empty, otherwhise render Title text field */}
+              {props.fields?.PageLink ? (
+                <Link field={props.fields?.PageLink} />
+              ) : (
+                <Text field={props.fields?.Title} />
+              )}
             </h3>
           </div>
           <div className="col-md-9">
             {/* Define editalbe rich text field to render Content field */}
-            <RichText field={fields?.Content} />
+            <RichText field={props.fields?.Content} />
           </div>
         </div>
       </div>
